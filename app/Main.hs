0682d230c72515c5adf3a8a1ec6089ff28229caa
William Wennerstr?m <william@willeponken.me>
2019-03-28 17:52:24 +0100

    app+src: seems to work for most (?) input

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,9 @@ module Main where
 import Lib
 import System.IO
 
-main :: IO ()
+main :: IO [()]
 main = do
   contents <- getContents
   let modules = parseGoModules contents
-  let deps = map toEgoDep modules
-  dirs <- getAbsDirectories "/home/willeponken/go/pkg/mod/cache/vcs/"
-  hash <- findLongHash dirs "golang.org/x/sys" "a34e9553db1e"
-  print dirs
-  print hash
+  deps <- mapM toEgoDep modules
+  mapM printEgoDep deps
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,15 @@
 module Lib where
 
 import Control.Monad (filterM)
-import Data.List (isSuffixOf)
+import Data.List (isPrefixOf, isSuffixOf)
 import GHC.IO.Exception (ExitCode(ExitSuccess))
-import System.Directory (canonicalizePath, doesDirectoryExist, listDirectory)
+import System.Directory
+  ( canonicalizePath
+  , doesDirectoryExist
+  , getHomeDirectory
+  , listDirectory
+  )
+import System.Environment (lookupEnv)
 import System.FilePath ((</>))
 import System.Process
   ( StdStream(CreatePipe)
@@ -17,14 +23,13 @@ import System.Process
 data GoModule = GoModule
   { path :: String
   , version :: String
-  , goMod :: String
   } deriving (Show) -- GoModule deriving _from_ Show (making it printable).
 
 getGoModule :: [String] -> GoModule
 -- Pattern matching:
--- Matches with ["something", "something", "something"], and then uses the
--- variables p, v and g to construct a Module.
-getGoModule [p, v, g] = GoModule {path = p, version = v, goMod = g}
+-- Matches with ["something", "something"], and then uses the
+-- variables p and v to construct a Module.
+getGoModule [p, v, _] = GoModule {path = p, version = v}
 
 parseGoModules :: String -> [GoModule]
 -- Dot operator (function composition):
@@ -37,15 +42,9 @@ parseGoModules c = map (getGoModule . words) (lines c)
 
 data EgoDep = EgoDep
   { name :: String
-  , hash :: String
-  , uri :: String
+  , rev :: String
+  , ref :: String
   } deriving (Show)
-
-toEgoDep :: GoModule -> EgoDep
--- Again, pattern matching:
--- Matches with the GoModule data, where `path` is set to be the same thing as
--- `n` and `version` is the same thing as `h`.
-toEgoDep GoModule {path = n, version = h} = EgoDep {name = n, hash = h, uri = n}
 
 getAbsDirectories :: FilePath -> IO [FilePath]
 getAbsDirectories filePath =
@@ -61,21 +60,74 @@ callCommand cwd cmd =
 
 getLongHash :: FilePath -> String -> IO String
 getLongHash path version = do
-  (_, hash, _) <- callCommand path ("git rev-parse " ++ version)
-  print hash
-  return (init hash) -- Drop last newline in rev-parse output.
+  (e, hash, _) <- callCommand path ("git rev-parse " ++ version)
+  if e == ExitSuccess
+    then return $ init hash -- Drop last newline in rev-parse output.
+    else return []
 
 getGoModHead :: FilePath -> String -> IO String
 getGoModHead path version = do
   (e, mod, _) <- callCommand path ("git show " ++ version ++ ":go.mod")
   if e == ExitSuccess
-    then return (init mod)
+    then return $ last $ words $ init mod -- Extract last word w/o newline.
+    else return [] -- Nothing found for version or go.mod doesn't exist.
+
+getGitRemote :: FilePath -> IO String
+getGitRemote path = do
+  (e, remote, _) <- callCommand path "git remote get-url origin"
+  if e == ExitSuccess
+    then return $ init remote -- Drop last newline in get-url output.
     else return []
 
-findLongHash :: [FilePath] -> String -> String -> IO String
-findLongHash [] _ _ = return [] -- No long hash found.
-findLongHash (path:paths) name version = do
+getGoPath :: IO FilePath
+getGoPath = do
+  (e, path, _) <- callCommand "/" "go env GOPATH"
+  if e == ExitSuccess
+    then return $ init path -- Drop last newline in go env output.
+    else do
+      home <- getHomeDirectory
+      return $ home </> "go"
+
+findGoModule :: [FilePath] -> String -> String -> IO (FilePath, String, String)
+findGoModule [] _ _ = return ([], [], []) -- No Go module found.
+findGoModule (path:paths) name version = do
   mod <- getGoModHead path version
-  if name `isSuffixOf` mod
-    then getLongHash path version
-    else findLongHash paths name version
+  hash <- getLongHash path version
+  remote <- getGitRemote path
+  if name `isSuffixOf` mod ||
+     version `isPrefixOf` hash || name `isSuffixOf` remote
+    then return (path, hash, remote)
+    else findGoModule paths name version
+
+versionToRev :: String -> String
+versionToRev version
+  | "+incompatible" `isSuffixOf` version = take (length version - 13) version
+  | length version == 34 = drop 22 version
+  | otherwise = version
+
+chooseRev :: String -> String -> String
+chooseRev rev hash
+  | hash /= "" = hash
+  | otherwise = rev
+
+toRef :: String -> String
+toRef = drop 8
+
+toEgoDep :: GoModule -> IO EgoDep
+-- Again, pattern matching:
+-- Matches with the GoModule data, where `path` is set to be the same thing as
+-- `name` and `version` is the same thing as `version`.
+toEgoDep GoModule {path = name, version = version} = do
+  let rev = versionToRev version
+  gopath <- getGoPath
+  dirs <- getAbsDirectories $ gopath </> "pkg/mod/cache/vcs/"
+  (dir, hash, remote) <- findGoModule dirs name rev
+  return EgoDep {name = name, rev = chooseRev rev hash, ref = toRef remote}
+
+printEgoDep :: EgoDep -> IO ()
+printEgoDep dep = putStrLn $ n ++ rv ++ rf
+  where
+    n = name dep
+    rv = " " ++ rev dep
+    rf
+      | rv /= ref dep = " " ++ ref dep
